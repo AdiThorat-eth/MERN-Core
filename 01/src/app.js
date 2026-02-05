0501f445a95a8828148ef823672e02ab1eca17cf
Aditya Thorat <a.workspace.00@gmail.com>
2026-02-05 14:58:16 +0530

    backend of mini project implemented

--- 01/src/app.js
+++ 01/src/app.js
@@ -27,11 +27,43 @@ app.post("/notes", async (req, res) => {
 });
 
 app.get("/notes", async (req, res) => {
-  const notes = await noteModel.find(); // find() will always return an array
-
+  // find() will always return an array of objects if case not found then empty array "[]"
+  const notes = await noteModel
+    .find
+    //     {
+    //     title: "tests_title",
+    //   }
+    ();
+  /*
+  // findOne() will always return an object if case not found then null "null"
+  const notes = await noteModel.findOne({
+  title: "test_title"
+  }); 
+  */
   res.status(200).json({
     message: "Notes fetched successfully",
     notes: notes,
+  });
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  await noteModel.findOneAndDelete({
+    _id: id,
+  });
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+app.patch("/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const description = req.body.description;
+
+  // findOneAndUpdate asks for two arguments first argument is the condition to find the document second argument is the data to update
+  await noteModel.findOneAndUpdate({ _id: id }, { description: description });
+  res.status(200).json({
+    message: "Note updated successfully",
   });
 });
 
