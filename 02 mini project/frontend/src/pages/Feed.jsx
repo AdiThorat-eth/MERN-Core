4ed555772857b4ba958405fcbf2f4ee519e69e45
Aditya Thorat <a.workspace.00@gmail.com>
2026-02-05 16:25:30 +0530

    mini project finished

---
+++ 02 mini project/frontend/src/pages/Feed.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Feed = () => {
+  const [posts, setPosts] = useState([
+    {
+      _id: "1",
+      image:
+        "https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+      caption: "goffy",
+    },
+  ]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts").then((res) => {
+      setPosts(res.data.posts);
+    });
+  }, []);
+
+  return (
+    <section className="feed-section">
+      {posts.length > 0 ? (
+        posts.map((post) => (
+          <div key={post._id} className="post-card">
+            <img src={post.image} alt={post.caption} />
+            <h3>{post.caption}</h3>
+          </div>
+        ))
+      ) : (
+        <p>No posts found</p>
+      )}
+    </section>
+  );
+};
+
+export default Feed;
